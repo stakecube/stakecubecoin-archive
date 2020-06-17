64c41b1aa1545b2281b3bb8a9514a0965ed4c51b
stakecube <olegwarkentin@gmx.net>
2020-06-17 11:36:08 +0200

    Always use quark algo

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -16,10 +16,7 @@
 
 uint256 CBlockHeader::GetHash() const
 {
-    if(nVersion < 4)
-        return HashQuark(BEGIN(nVersion), END(nNonce));
-
-    return Hash(BEGIN(nVersion), END(nNonce));
+    return HashQuark(BEGIN(nVersion), END(nNonce));
 }
 
 uint256 CBlock::BuildMerkleTree(bool* fMutated) const
