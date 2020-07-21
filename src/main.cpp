0fe87f984e6ff78501af0291f75875e666a22456
JSKitty <loopylew113@protonmail.com>
2020-07-21 12:01:36 +0100

    [Core] Only check blockindex when necessary
    This stops CheckBlockIndex() being spammed during IDB and chain syncing, by using the Checkpoints estimation to ensure the majority of the chain is synced quickly, with only newer blocks being checked for consistency at a 100-block interval on Mainnet. Testnets will skip this and always enforce CheckBlockIndex()

--- src/main.cpp
+++ src/main.cpp
@@ -2907,7 +2907,10 @@ bool ActivateBestChain(CValidationState& state, CBlock* pblock, bool fAlreadyChe
             }
         }
     } while (pindexMostWork != chainActive.Tip());
-    CheckBlockIndex();
+
+    // Check the Block Index on a non-intensive interval to prevent massive sync slowdowns
+    if (!IsInitialBlockDownload() && pindexNewTip->nHeight % 100 == 0 && Checkpoints::GuessVerificationProgress(pindexNewTip) > 0.997 || Params().NetworkID() != CBaseChainParams::MAIN)
+        CheckBlockIndex();
 
     // Write changes periodically to disk, after relay.
     if (!FlushStateToDisk(state, FLUSH_STATE_PERIODIC)) {
@@ -3878,7 +3881,6 @@ bool ProcessNewBlock(CValidationState& state, CNode* pfrom, CBlock* pblock, CDis
         if (pindex && pfrom) {
             mapBlockSource[pindex->GetBlockHash ()] = pfrom->GetId ();
         }
-        CheckBlockIndex ();
         if (!ret)
             return error ("%s : AcceptBlock FAILED", __func__);
     }
@@ -5525,7 +5527,8 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
             pfrom->PushMessage(NetMsgType::GETHEADERS, chainActive.GetLocator(pindexLast), uint256(0));
         }
 
-        CheckBlockIndex();
+        if (!IsInitialBlockDownload() && pindexLast && pindexLast->nHeight % 100 == 0 && Checkpoints::GuessVerificationProgress(pindexLast) > 0.997 || Params().NetworkID() != CBaseChainParams::MAIN)
+            CheckBlockIndex();
     }
 
     else if (strCommand == NetMsgType::BLOCK && !fImporting && !fReindex) // Ignore blocks received while importing
